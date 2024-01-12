7e22d3b2947af58e8fca27792e3a471821c9db10
Blessing Awak <melissasignature1@gmail.com>
2024-01-12 18:52:38 +0100

    commit

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ update();
 
 function update() {
   let date = new Date();
-  myLabel.innerHTML = date;
+  myLabel.innerHTML = formatTime(date);
 
-  
+  function formatTime(date) {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    let amOrPm = hours >= 12 ? "pm" : "am";
+
+    hours = hours % 12 || 12; 
+
+    return `${hours}:${minutes}:${seconds} ${amOrPm}`;
+  }
 }
